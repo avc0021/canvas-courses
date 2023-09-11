239772f3e679fb2a191d384c1f0fc8c042f951d3
AdamCWork <ajcastr1@uiwtx.edu>
2023-09-11 10:04:25 -0500

    uploaded to prod, waiting on graphql/ ethos to be configured. card is currently giving ethos error upon fetch for bannerId

--- extension.js
+++ extension.js
@@ -7,13 +7,45 @@ module.exports = {
         "title": "Canvas",
         "displayCardType": "Canvas Card",
         "description": "This card displays data from Canvas",
-        // "configuration": {
-        //     client: [{
-        //         key: 'lambdaUrl',
-        //         label: 'Lambda URL',
-        //         type: 'text',
-        //         required: true
-        //     }]
-        // }
+        queries: {
+            'list-persons': [
+                {
+                    resourceVersions: {
+                        persons: { min: 12 }
+                    },
+                    query:
+                        `query listPerson($personId: ID){
+                            persons: {persons} (
+                                filter: {id: {EQ: $personId}}
+                                ) 
+                                {
+                                    edges {
+                                        node {
+                                            id
+                                                credentials {
+                                                    type
+                                                    value
+                                                }
+                                                names {
+                                                    firstName
+                                                    lastName
+                                                    middleName
+                                                    preference
+                                                    fullName
+                                                }
+                                                emails {
+                                                    type {
+                                                        emailType
+                                                    }
+                                                    address
+                                                    preference
+                                                }
+                                        }
+                                    }
+                                }
+                        }`
+                }
+            ]
+        }
     }]
 }
